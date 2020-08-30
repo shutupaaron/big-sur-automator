1daa45588abd9d7590fc80ed0651b91b123cbc66
아잇 <68202470+nonforgettable@users.noreply.github.com>
2020-08-30 16:44:38 +0900

    Update shellfile0.1.9dev6.sh

--- testing grounds/shellfile0.1.9dev6.sh
+++ testing grounds/shellfile0.1.9dev6.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-echo To make this script work please set a unix password by entering your password twice
-sudo passwd
-echo enter password
-su
 echo big sur automator nonrelease 019
-sudo curl -o /Users/Shared/InstallAssistant.pkg --url http://swcdn.apple.com/content/downloads/34/37/001-26097-A_OYW4Z0U99Z/gdas3ubse9fasb0217grmmyplfd6o7mqfp/InstallAssistant.pkg
-sudo installer -store -pkg "/Users/Shared/InstallAssistant.pkg" -target /
-sudo /Applications/Install\ macOS\ Big\ Sur\ Beta.app/Contents/Resources/createinstallmedia --nointeraction --volume /Volumes/volume
+curl -o /Users/Shared/InstallAssistant.pkg --url http://swcdn.apple.com/content/downloads/34/37/001-26097-A_OYW4Z0U99Z/gdas3ubse9fasb0217grmmyplfd6o7mqfp/InstallAssistant.pkg
+installer -store -pkg "/Users/Shared/InstallAssistant.pkg" -target /
+/Applications/Install\ macOS\ Big\ Sur\ Beta.app/Contents/Resources/createinstallmedia --nointeraction --volume /Volumes/volume
 git clone https://www.github.com/barrykn/big-sur-micropatcher
-sudo sh ~/big-sur-micropatcher/micropatcher.sh
-sudo nvram boot-args="-no_compat_check amfi_get_out_of_my_way" echo Success
-sudo bless -mount /Volumes/Install\ macOS\ Big\ Sur\ Beta --setBoot
-sudo rm -rf ~/big-sur-micropatcher
-sudo rm -rf /Users/Shared/InstallAssistant.pkg
+sh ~/big-sur-micropatcher/micropatcher.sh
+nvram boot-args="-no_compat_check amfi_get_out_of_my_way" echo Success
+ bless -mount /Volumes/Install\ macOS\ Big\ Sur\ Beta --setBoot
+ rm -rf ~/big-sur-micropatcher
+ rm -rf /Users/Shared/InstallAssistant.pkg
 echo success
 
 
