e5a9fdbdef2ead9a0b5839ef7c85e7bab1ab861c
아잇 <68202470+nonforgettable@users.noreply.github.com>
2020-09-01 01:57:13 +0900

    Update nightlybsa.sh

--- nightlybsa.sh
+++ nightlybsa.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e “e[0;33mBig Sur Automator 109 canary[0m”
+echo -e “e[0;33mBig Sur Automator X Stable[0m”
 curl -o /Users/Shared/InstallAssistant.pkg --url http://swcdn.apple.com/content/downloads/34/37/001-26097-A_OYW4Z0U99Z/gdas3ubse9fasb0217grmmyplfd6o7mqfp/InstallAssistant.pkg
 installer -store -pkg "/Users/Shared/InstallAssistant.pkg" -target /
 /Applications/Install\ macOS\ Big\ Sur\ Beta.app/Contents/Resources/createinstallmedia --nointeraction --volume /Volumes/volume
