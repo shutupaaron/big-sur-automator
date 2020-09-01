0220d8e17c82829063c23e7e5dea84fdcc313287
아잇 <68202470+nonforgettable@users.noreply.github.com>
2020-09-01 23:47:21 +0900

    Update nightlybsa.sh

--- nightlybsa.sh
+++ nightlybsa.sh
@@ -5,7 +5,7 @@ installer -store -pkg "/Users/Shared/InstallAssistant.pkg" -target /
 /Applications/Install\ macOS\ Big\ Sur\ Beta.app/Contents/Resources/createinstallmedia --nointeraction --volume /Volumes/volume
 git clone https://www.github.com/barrykn/big-sur-micropatcher
 sh ~/big-sur-micropatcher/micropatcher.sh
-nvram boot-args="-no_compat_check amfi_get_out_of_my_way" echo Success
+nvram boot-args="-no_compat_check amfi_get_out_of_my_way" 
 bless -mount /Volumes/Install\ macOS\ Big\ Sur\ Beta --setBoot
 echo to delete the downloaded files check the readme
 echo success
